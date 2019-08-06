4a8f2347813f4884717db868f5f6cd0af6b2d2ee
thetianshuhuang <thetianshuhuang@gmail.com>
2019-08-05 21:09:22 -0500

    Finished rework of photography (now mobile friendly!)

--- templates/render.py
+++ templates/render.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 
-def render(target, context=[]):
+def render(target, context=[], path=[]):
 
     with open(target) as f:
         template = Template(f.read())
@@ -13,11 +13,35 @@ def render(target, context=[]):
     context = {f: pd.read_csv(f + '.csv') for f in context}
 
     with open(os.path.join('..', target), 'w') as res:
-        res.write(template.render(base=base, **context))
+        res.write(template.render(base=base, path=path, **context))
 
 
 if __name__ == '__main__':
-    render("photography.html", context=["landscapes", "panoramas"])
-    render("architecture.html")
-    render("miscellaneous.html")
+    render(
+        "panoramas.html",
+        context=["panoramas"],
+        path=[
+            ['photography.html', 'Photography'],
+            ['panoramas.html', 'Panoramas']
+        ])
+    render(
+        "photos.html",
+        context=["photos"],
+        path=[
+            ['photography.html', 'Photography'],
+            ['photos.html', 'Photos']
+        ])
+    render(
+        "photography.html",
+        path=[['photography.html', 'Photography']])
+    render(
+        "architecture.html",
+        path=[['architecture.html', 'Architecture']])
+    render(
+        "miscellaneous.html",
+        path=[['miscellaneous.html', 'Miscellaneous']])
     render("index.html")
+    render(
+        "resources.html",
+        context=["resources"],
+        path=[['resources.html', 'Resources']])
