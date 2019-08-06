5153a5f6c86b861c8df5aca831c20e0812452b5e
thetianshuhuang <thetianshuhuang@gmail.com>
2019-08-06 13:20:46 -0500

    Finished redoing project page

--- templates/render.py
+++ templates/render.py
@@ -3,22 +3,108 @@ import pandas as pd
 import os
 
 
-def render(target, context=[], path=[]):
+def render(target, context=[], path=[], title=None, process_context=None):
 
     with open(target) as f:
         template = Template(f.read())
     with open('base.html') as f:
         base = Template(f.read())
 
-    context = {f: pd.read_csv(f + '.csv') for f in context}
+    context = {
+        f: pd.read_csv(os.path.join('definitions', f + '.csv'))
+        for f in context
+    }
+
+    if process_context is not None:
+        context = process_context(context)
 
     with open(os.path.join('..', target), 'w') as res:
-        res.write(template.render(base=base, path=path, **context))
+        res.write(template.render(
+            base=base,
+            path=path,
+            pagetitle="" if title is None else " | " + title,
+            **context))
+
+
+def get_folder_desc(folder):
+    with open(os.path.join(folder, 'desc.txt')) as f:
+        return f.read()
+
+
+def get_images(ctx):
+
+    return {
+        "projects": [
+            {
+                "name": project["name"],
+                "date": project["date"],
+                "desc": get_folder_desc(
+                    os.path.join("..", "renders", project["folder"])),
+                "folder": project["folder"],
+                "images": [
+                    f for f in
+                    os.listdir(
+                        os.path.join("..", "renders", project["folder"]))
+                    if f != 'desc.txt'
+                ]
+            } for _, project in ctx["architecture"].iterrows()
+        ]
+    }
+
+
+def get_project_desc(name):
+    with open(os.path.join('definitions', 'project_descriptions', name)) as f:
+        return f.readlines()
+
+
+def nan_to_null(x):
+    if pd.isnull(x):
+        return None
+    else:
+        return x
+
+
+TAG_MAP = {
+    "embedded": "Embedded Systems",
+    "util": "Utility",
+    "lib": "Library",
+    "python": "Language:Python",
+    "c": "Language:C",
+    "javascript": "Language:JavaScript",
+    "st3": "Plugin:Sublime Text 3",
+    "class": "Class Project",
+    "web": "Web",
+    "ds": "Data Science",
+    "arch": "Computer Architecture"
+}
+
+
+def tag_map(t):
+    try:
+        return TAG_MAP[t]
+    except KeyError:
+        return t
+
+
+def get_descriptions(ctx):
+    return {
+        "projects": [
+            {
+                "name": project["name"],
+                "organization": nan_to_null(project["organization"]),
+                "icon": project["icon"],
+                "tags": [tag_map(t) for t in project["tag"].split(',')],
+                "link": project["link"],
+                "desc": get_project_desc(project["description"])
+            } for _, project in ctx["projects"].iterrows()
+        ]
+    }
 
 
 if __name__ == '__main__':
     render(
         "panoramas.html",
+        title="Panoramas",
         context=["panoramas"],
         path=[
             ['photography.html', 'Photography'],
@@ -26,6 +112,7 @@ if __name__ == '__main__':
         ])
     render(
         "photos.html",
+        title="Photos",
         context=["photos"],
         path=[
             ['photography.html', 'Photography'],
@@ -33,15 +120,27 @@ if __name__ == '__main__':
         ])
     render(
         "photography.html",
+        title="Photography",
         path=[['photography.html', 'Photography']])
     render(
         "architecture.html",
-        path=[['architecture.html', 'Architecture']])
+        title="Architecture",
+        context=["architecture"],
+        path=[['architecture.html', 'Architecture']],
+        process_context=get_images)
     render(
         "miscellaneous.html",
+        title="Miscellaneous",
         path=[['miscellaneous.html', 'Miscellaneous']])
     render("index.html")
     render(
         "resources.html",
+        title="Resources",
         context=["resources"],
         path=[['resources.html', 'Resources']])
+    render(
+        "projects.html",
+        title="Projects",
+        context=['projects'],
+        path=[['projects.html', 'Projects']],
+        process_context=get_descriptions)
