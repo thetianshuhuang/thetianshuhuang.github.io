ea181f4f309ca9b042a200e812e74e4d7fd5a9de
Tianshu Huang <thetianshuhuang@gmail.com>
2025-05-24 11:39:34 -0400

    Add a few photos + year dividers; also tyro-ified the scripts

--- compress.py
+++ compress.py
@@ -2,15 +2,12 @@
 
 import os
 
+import tyro
 import yaml
 from PIL import Image
+from tqdm import tqdm
 
 Image.MAX_IMAGE_PIXELS = None
-THUMB_SIZE = 320
-PHOTO_SIZE = 1 * 1000 * 1000
-
-with open("data/photos.yaml") as f:
-    photos = yaml.load(f, Loader=yaml.Loader)["photos"]
 
 
 def _crop(short, long, target):
@@ -24,7 +21,9 @@ def _crop(short, long, target):
         return lower, upper
 
 
-def _compress(src):
+def _compress(
+    src, thumb_size: int = 320, photo_size: int = 1000 * 1000
+) -> None:
     img = Image.open(os.path.join("raw", src['path']))
 
     # Thumbnail
@@ -36,21 +35,54 @@ def _compress(src):
     else:
         left, right = _crop(img.size[1], img.size[0], src['thumb'])
         cropped = img.crop((left, 0, right, img.size[1]))
-    thumb = cropped.resize((THUMB_SIZE, THUMB_SIZE), Image.Resampling.LANCZOS)
+    thumb = cropped.resize((thumb_size, thumb_size), Image.Resampling.LANCZOS)
     thumb.save(os.path.join("thumbs", src["path"]), 'jpeg', quality=75)
 
     # Display
     aspect = max(img.size) / min(img.size)
-    target_mp = PHOTO_SIZE * aspect
+    target_mp = photo_size * aspect
     scale = (target_mp / (img.size[0] * img.size[1])) ** 0.5
     scaled = (int(img.size[0] * scale), int(img.size[1] * scale))
     display = img.resize(scaled, Image.Resampling.LANCZOS)
     display.save(os.path.join("photos", src["path"]), 'jpeg', quality=75)
 
 
-for src in photos:
-    try:
-        _compress(src)
-        print("Compressed: {}".format(src['path']))
-    except FileNotFoundError:
-        print("Skipping: {} (source not available)".format(src['path']))
+def compress(
+    index: str = "data/photos.yaml",
+    thumb: int = 320,
+    photo: int = 1000 * 1000,
+    overwrite: bool = False
+) -> None:
+    """Compress / downsample images, and generate thumbnails.
+
+    Args:
+        index: photo index file.
+        thumb: thumbnail width/height, in pixels; thumbnails are always square.
+        photo: compressed photo size, in total pixels.
+        overwrite: whether to re-compress existing images.
+    """
+    with open(index) as f:
+        photo_index = yaml.load(f, Loader=yaml.Loader)["photos"]
+
+    skipped, na, success = [], [], []
+    photos = sum(photo_index.values(), [])
+    for src in tqdm(photos):
+        if overwrite or not os.path.exists(os.path.join("thumbs", src['path'])):
+            try:
+                _compress(src, thumb_size=thumb, photo_size=photo)
+                success.append(src['path'])
+            except FileNotFoundError:
+                na.append(src['path'])
+        else:
+            skipped.append(src['path'])
+
+    if len(success) > 0:
+        print(f"Compressed {len(success)} images: {success}")
+    if len(skipped) > 0:
+        print(f"Skipped {len(skipped)} already-compressed images: {skipped}")
+    if len(na) > 0:
+        print(f"Source unavailable for {len(na)} images: {na}")
+
+
+if __name__ == "__main__":
+    tyro.cli(compress)
--- render.py
+++ render.py
@@ -2,68 +2,86 @@
 
 import os
 
+import tyro
 import yaml
 from jinja2 import Template
 
-TEMPLATE_PATH = "template"
-TEMPLATE_BASE = "template/page.html"
-PAPER_TEMPLATE = "template/paper.html"
-PAPERS_DIR = "papers"
 
-with open(TEMPLATE_BASE) as f:
-    base = Template(f.read())
+def _load_context(path: str = "data") -> dict:
+    context = {}
+    for path in os.listdir(path):
+        with open(os.path.join("data", path)) as f:
+            context.update(yaml.load(f, Loader=yaml.Loader))
 
+    def _linkto(name: str, mode: str = 'simple') -> str:
+        name, affiliations, *_ = (name + ':').split(':')
 
-context = {}
-for path in os.listdir("data"):
-    with open(os.path.join("data", path)) as f:
-        context.update(yaml.load(f, Loader=yaml.Loader))
+        if mode != 'full':
+            affiliations = '*' if '*' in affiliations else ''
+            if mode == 'cv' and name.startswith('Tianshu Huang'):
+                return f'<b>{name}{affiliations}</b>'
 
+        try:
+            res = f'<a href="{context["people"][name]}">{name}</a>'
+        except KeyError:
+            res = name
 
-def _linkto(name: str, mode: str = 'simple') -> str:
-    name, affiliations, *_ = (name + ':').split(':')
+        if len(affiliations) > 0:
+            res += f"<sup>{affiliations}</sup>"
+        return res
 
-    if mode != 'full':
-        affiliations = '*' if '*' in affiliations else ''
-        if mode == 'cv' and name.startswith('Tianshu Huang'):
-            return f'<b>{name}{affiliations}</b>'
-
-    try:
-        res = f'<a href="{context["people"][name]}">{name}</a>'
-    except KeyError:
-        res = name
-
-    if len(affiliations) > 0:
-        res += f"<sup>{affiliations}</sup>"
-    return res
+    context["ref"] = _linkto
+    return context
 
 
-context["ref"] = _linkto
-targets = {
-    "index.html": None,
-    "photography.html": "Photography",
-    "404.html": "Not Available"
-}
+def render(
+    templates: str = "./template", papers: str = "./papers", data: str = "data"
+) -> None:
+    """Render website templates.
 
-for target, title in targets.items():
-    with open(os.path.join(TEMPLATE_PATH, target)) as f:
+    Args:
+        templates: template file directory.
+        papers: paper sub-templates.
+        data: data file directory.
+    """
+    TEMPLATE_BASE = os.path.join(templates, "page.html")
+    PAPER_TEMPLATE = os.path.join(templates, "paper.html")
+
+    with open(TEMPLATE_BASE) as f:
+        base = Template(f.read())
+
+    context = _load_context(data)
+    targets = {
+        "index.html": None,
+        "photography.html": "Photography",
+        "404.html": "Not Available"
+    }
+
+    for target, title in targets.items():
+        with open(os.path.join(templates, target)) as f:
+            template = Template(f.read())
+        contents = template.render(**context)
+        extras = {
+            "pagefile":  target, "pagetitle": title, "contents": contents}
+        rendered = base.render(**context, **extras)
+        with open(target, 'w') as f:
+            f.write(rendered)
+
+    with open(PAPER_TEMPLATE) as f:
         template = Template(f.read())
-    contents = template.render(**context)
-    extras = {"pagefile":  target, "pagetitle": title, "contents": contents}
-    rendered = base.render(**context, **extras)
-    with open(target, 'w') as f:
-        f.write(rendered)
 
-with open(PAPER_TEMPLATE) as f:
-    template = Template(f.read())
+    for paper in os.listdir(papers):
+        paper_context = context["papers"][paper.split('.')[0]]
 
-for paper in os.listdir(PAPERS_DIR):
-    paper_context = context["papers"][paper.split('.')[0]]
+        with open(os.path.join(papers, paper)) as f:
+            content = f.read()
 
-    with open(os.path.join(PAPERS_DIR, paper)) as f:
-        content = f.read()
+        rendered = template.render(
+            contents=content, **paper_context, **context)
 
-    rendered = template.render(contents=content, **paper_context, **context)
+        with open(paper, 'w') as f:
+            f.write(rendered)
 
-    with open(paper, 'w') as f:
-        f.write(rendered)
+
+if __name__ == '__main__':
+    tyro.cli(render)
